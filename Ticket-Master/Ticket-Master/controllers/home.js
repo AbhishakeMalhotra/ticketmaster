e250914db6c485d3f2a83f54ec57b5c154f92fa1
Fouad Ouardi <86013594+fouadsphone@users.noreply.github.com>
2022-08-13 22:18:42 -0400

    Delete Ticket-Master/Ticket-Master directory

--- Ticket-Master/Ticket-Master/controllers/home.js
+++
@@ -1,9 +0,0 @@
-
-module.exports.homelist = function(req, res, next) {  
-   
-    res.render('home', { 
-        title: 'Home',
-        userName: req.user ? req.user.username : ''
-    })           
-     
-}
--- Ticket-Master/Ticket-Master/routes/home.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-let controlerHome = require('../controllers/home');
-
-/* GET home page. */
-router.get('/', controlerHome.homelist);
-
-module.exports = router;
-
